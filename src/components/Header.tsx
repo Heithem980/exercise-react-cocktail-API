80bb25e03615943d77ddf0eec3702bfcad8ca433
Heithem980 <heithem.gha98@gmail.com>
2024-08-23 10:19:18 +0200

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,14 @@ import "../css/Header.css";
 
 export function Header(): ReactElement {
     return (
-        <header className="header">
-        <p className="logo">Cocktail wiki</p>
-        <nav className="navbar">
-          <a className="link" href="/">Home</a>
-          <a className="link" href="/add-todo">Add</a>
-          <a className="link" href="">hmm</a>
-        </nav>
-      </header>
+      <header className="header">
+        <h1>Cocktail-wiki</h1>
+      <nav className="nav">
+        <a href="/" className="link">Home</a>
+        <a href="/about" className="link">Search</a>
+        
+      </nav>
+    </header>
     );
   }
   
