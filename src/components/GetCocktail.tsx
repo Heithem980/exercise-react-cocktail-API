d713612c5afa0a59f962c7c40b483e66e96071df
Heithem980 <heithem.gha98@gmail.com>
2024-08-24 19:48:42 +0200

    working on landing page

--- src/components/GetCocktail.tsx
+++ src/components/GetCocktail.tsx
@@ -1,27 +1,36 @@
 import { useState, useEffect } from "react";
-
-
-
-
-
-
-
+import "../css/CocktailCard.css";
 
 export function GetRandomCocktail() {
   const [CocktailImage, setCocktailImage] = useState(null);
   const [CocktailName, setCocktailName] = useState(null);
+  const [triggerEffect, setTriggerEffect] = useState(0);
 
   useEffect(() => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then((response) => response.json())
       .then((data) => {
         setCocktailImage(data.drinks[0].strDrinkThumb);
+        setCocktailName(data.drinks[0].strDrink);
       });
-  }, []);
+  }, [triggerEffect]);
+
+  const GetRandomCocktailOnClick = () => {
+    setTriggerEffect((count) => count + 1);
+  };
 
   return (
-    <div className="card">
-      {CocktailImage && <img src={CocktailImage}></img>}
-    </div>
+    <>
+      <div className="cardContainer">
+        <div className="card">
+          {CocktailImage && <img className="image" src={CocktailImage}></img>}
+          <p>{CocktailName}</p>
+        </div>
+      </div>
+      <div className="buttondiv">
+      <button onClick={GetRandomCocktailOnClick}>Randomize</button>
+      </div>
+      
+    </>
   );
 }
